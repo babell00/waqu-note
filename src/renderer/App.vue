3eab2af024b193501240c6c4183a76be3f117cb0
Jakub G <gajzler.jakub@gmail.com>
2018-08-04 19:01:12 +0200

    removing git ignore

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+{{test}}
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            test: 'Vue is working'
+        }
+    }
+}
+</script>
