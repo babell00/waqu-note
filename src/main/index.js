648cd55c460b5a6d5a55a759425979601c0328c5
Jakub G <gajzler.jakub@gmail.com>
2018-08-04 18:53:21 +0200

    init project

---
+++ src/main/index.js
@@ -0,0 +1,7 @@
+import {app , BrowserWindow} from 'electron'
+
+app.on('ready', () => {
+  let window = new BrowserWindow({});
+  window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+  
+})
