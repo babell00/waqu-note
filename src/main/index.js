9bb36adb91c68b03be8a4c6cf959dd429133e554
Jakub G <gajzler.jakub@gmail.com>
2018-08-04 19:45:57 +0200

    ready to go

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,20 @@
 import {app , BrowserWindow} from 'electron'
+import { format as formatUrl } from 'url'
+import path from 'path'
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 app.on('ready', () => {
   let window = new BrowserWindow({});
-  window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+  if(isDev){
+    window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
+  } else {
+    window.loadURL(formatUrl({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file',
+      slashes: true
+    }));
+  }
+  
   
 })
